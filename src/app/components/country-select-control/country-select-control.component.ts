b54d542ed22270c3429f25c7a66bfd47f4c57228
Ivan Siderov <ivan.siderov@accedia.com>
2019-02-21 10:55:14 +0200

    added initial project

---
+++ src/app/components/country-select-control/country-select-control.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap, map, startWith } from 'rxjs/operators';
+let countriesJSON = require('./countries.json')
+
+@Component({
+  selector: 'country-select-control',
+  templateUrl: './country-select-control.component.html',
+  styleUrls: ['./country-select-control.component.scss']
+})
+export class CountrySelectControlComponent {
+  public countries: any = countriesJSON;
+  public filteredCountries: Observable<any>;
+  public myControl: FormControl = new FormControl();
+
+  ngOnInit() {
+    this.filteredCountries = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  public getCountryViewValue(country: any) {
+    return country
+      ? country.value
+      : '';
+  }
+
+  private _filter(value: string): any {
+    if (typeof value !== 'string') return;
+
+    const filterValue = value.toLowerCase();
+    let filteredEntries = Object.entries(this.countries).filter((entry: any) => {
+      return entry[1].toLowerCase().includes(filterValue);
+    })
+
+    let filteredEntriesObject = filteredEntries.reduce((accumulator, current) => {
+      return Object.assign(accumulator, { [current[0]]: current[1] })
+    }, {})
+
+    return filteredEntriesObject
+  }
+}
---
+++ src/app/modules/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material';
+
+// Routing
+import { AppRoutingModule } from '../routing/app-routing.module';
+
+// Components
+import { AppComponent } from '../components/app/app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { TimelineComponent } from '../components/timeline/timeline.component';
+import { CountrySelectControlComponent } from '../components/country-select-control/country-select-control.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TimelineComponent,
+    CountrySelectControlComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
